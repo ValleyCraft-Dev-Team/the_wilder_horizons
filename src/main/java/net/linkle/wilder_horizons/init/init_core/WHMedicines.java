fb52636ce3c900d6d30a998d309427083b2a5507
andeditor7 <andeditor7@gmail.com>
2023-06-10 17:15:30 +1000

    Make the registering codes more legible

--- src/main/java/net/linkle/wilder_horizons/init/init_core/WHMedicines.java
+++ src/main/java/net/linkle/wilder_horizons/init/init_core/WHMedicines.java
@@ -2,6 +2,7 @@ package net.linkle.wilder_horizons.init.init_core;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.linkle.wilder_horizons.Main;
+import net.linkle.wilder_horizons.enums.ItemEnum;
 import net.linkle.wilder_horizons.init.init_exterior.WHGroups;
 import net.linkle.wilder_horizons.item.*;
 import net.minecraft.entity.effect.StatusEffects;
@@ -9,57 +10,62 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class WHMedicines {
-    /**bolus base - beetroot**/
+import java.util.Locale;
 
-    public static final Item BOLUS_POISON = registerItem("poison_bolus",
-            new BolusItem(new FabricItemSettings().maxCount(16).group(WHGroups.WH_MEDICINAL), 0,0, true, StatusEffects.NAUSEA));
+public enum WHMedicines implements ItemEnum {
 
-    /**salve base - dried kelp**/
+    // bolus base - beetroot
+    BOLUS_POISON(new BolusItem(settings().maxCount(16), 0,0, true, StatusEffects.NAUSEA)),
 
-    public static final Item SALVE_HEALING = registerItem("healing_salve",
-            new SalveItem(new FabricItemSettings().maxCount(16).group(WHGroups.WH_MEDICINAL), 0, 0,
-                    true, StatusEffects.REGENERATION));
+    // salve base - dried kelp
+    SALVE_HEALING(new SalveItem(settings().maxCount(16), 0, 0, true, StatusEffects.REGENERATION)),
 
-    /**tea base - glow berries**/
+    // tea base - glow berries
+    TEA_MUG(new AlphaModItem(settings().group(WHGroups.WH_MISCELLANEOUS))),
 
-    public static final Item TEA_MUG = registerItem("tea_mug",
-            new AlphaModItem(new FabricItemSettings().maxCount(64).group(WHGroups.WH_MISCELLANEOUS)));
+    TEA_HEALING(new BrothItem(settings().maxCount(1).group(WHGroups.WH_FOOD), 2, 0.8f, false, StatusEffects.STRENGTH)),
 
-    public static final Item TEA_HEALING = registerItem("tea_healing",
-            new BrothItem(new FabricItemSettings().maxCount(1).group(WHGroups.WH_FOOD), 2, 0.8f,
-                    false, StatusEffects.STRENGTH));
+    TEA_NIGHT_VISION(new BrothItem(settings().maxCount(1).group(WHGroups.WH_FOOD), 2, 0.8f, false, StatusEffects.NIGHT_VISION)),
 
-    public static final Item TEA_NIGHT_VISION = registerItem("tea_night_vision",
-            new BrothItem(new FabricItemSettings().maxCount(1).group(WHGroups.WH_FOOD), 2, 0.8f,
-                    false, StatusEffects.NIGHT_VISION));
+    TEA_HASTE(new BrothItem(settings().maxCount(1).group(WHGroups.WH_FOOD), 2, 0.8f, false, StatusEffects.HASTE)),
 
-    public static final Item TEA_HASTE = registerItem("tea_strength",
-            new BrothItem(new FabricItemSettings().maxCount(1).group(WHGroups.WH_FOOD), 2, 0.8f,
-                    false, StatusEffects.HASTE));
+    // ointment base - slime
+    OINTMENT_EXTINGUISHING(new SalveItem(settings().maxCount(16), 0, 0, true, StatusEffects.FIRE_RESISTANCE)),
 
-    /**ointment base - slime**/
-    public static final Item OINTMENT_EXTINGUISHING = registerItem("extinguishing_ointment",
-            new SalveItem(new FabricItemSettings().maxCount(16).group(WHGroups.WH_MEDICINAL), 0, 0,
-                    true, StatusEffects.FIRE_RESISTANCE));
+    OINTMENT_PROTECTING(new OintmentItem(settings().maxCount(16), 0, 0, true, StatusEffects.RESISTANCE)),
 
-    public static final Item OINTMENT_PROTECTING = registerItem("warding_ointment",
-            new OintmentItem(new FabricItemSettings().maxCount(16).group(WHGroups.WH_MEDICINAL), 0, 0,
-                    true, StatusEffects.RESISTANCE));
+    OINTMENT_CONCEALING(new OintmentItem(settings().maxCount(16), 0, 0, true, StatusEffects.INVISIBILITY)),
 
-    public static final Item OINTMENT_CONCEALING = registerItem("concealing_ointment",
-            new OintmentItem(new FabricItemSettings().maxCount(16).group(WHGroups.WH_MEDICINAL), 0, 0,
-                    true, StatusEffects.INVISIBILITY));
+    // soap base - clay
+    SOAP(new SoapItem(settings(), 0, 0, true, WHEffects.CLEANSED));
 
-    /**soap base - clay**/
-
-    public static final Item SOAP = registerItem("soap",
-            new SoapItem(new FabricItemSettings().maxCount(64).group(WHGroups.WH_MEDICINAL), 0, 0, true,
-                    WHEffects.CLEANSED));
-
+    private static FabricItemSettings settings() {
+        return new FabricItemSettings().group(WHGroups.WH_MEDICINAL);
+    }
+    
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Main.ID, name), item);
     }
     public static void registerItems() {
+    }
+
+    // ### Enum Codes ###
+
+    public final Item item;
+    public final Identifier id;
+
+    WHMedicines(Item item) {
+        this.id = Main.makeId(name().toLowerCase(Locale.ROOT));
+        this.item = Registry.register(Registry.ITEM, id, item);
+    }
+
+    @Override
+    public Item asItem() {
+        return item;
+    }
+
+    @Override
+    public Identifier getId() {
+        return id;
     }
 }
