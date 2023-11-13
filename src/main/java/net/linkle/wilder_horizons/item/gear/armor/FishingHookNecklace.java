f48868188f0b651d7fb7120d514200ca0439a06b
Linkle [they/them] <90809837+l1nkl3@users.noreply.github.com>
2023-11-12 22:51:22 -0800

    fixes and removals

--- src/main/java/net/linkle/wilder_horizons/item/gear/armor/FishingHookNecklace.java
+++ src/main/java/net/linkle/wilder_horizons/item/gear/armor/FishingHookNecklace.java
@@ -1,6 +1,5 @@
 package net.linkle.wilder_horizons.item.gear.armor;
 
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -9,11 +8,7 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
-
-import java.util.List;
 
 public class FishingHookNecklace extends ArmorItem {
 
@@ -24,7 +19,7 @@ public class FishingHookNecklace extends ArmorItem {
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         if (slot == EquipmentSlot.CHEST.getEntitySlotId() && entity instanceof LivingEntity living) {
-            living.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 3 * 20, 1, true, false, true));
+            living.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 3 * 20, 0, true, false, true));
         }
     }
 }
